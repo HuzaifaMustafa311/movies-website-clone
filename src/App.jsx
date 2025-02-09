44ff0f76e7565a818fd509253e1f01611ac61d6a
Huzaifa Mustafa <65065790+HuzaifaMustafa311@users.noreply.github.com>
2025-02-09 23:35:15 +0500

    happy

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,32 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import SimpleBottomNavigation from './components/MainNav'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Container, Switch } from '@mui/material'
+import Trending from './components/Pages/Trending/Trending'
+import Movies from './components/Pages/Movies/Movies'
+import Series from './components/Pages/Series/Series'
+import Search from './components/Pages/Search/Search'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <div className='app'>uiids</div>
-      <SimpleBottomNavigation />
+      <BrowserRouter>
+        <Header />
+        <div className='app'>
+          <Container>
+            <Switch>
+              <Route path='/' Component={Trending} exact />
+              <Route path='/movies' Component={Movies} />
+              <Route path='/series' Component={Series} />
+              <Route path='/search' Component={Search} />
+            </Switch>
+          </Container>
+        </div>
+        <SimpleBottomNavigation />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -7,10 +7,18 @@ import MovieIcon from '@mui/icons-material/Movie';
 import SearchIcon from '@mui/icons-material/Search';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import TvIcon from '@mui/icons-material/Tv';
+import { useEffect } from 'react';
 
 
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
+
+    useEffect(() => {
+        if (value === 0) history.push('/')
+        if (value === 1) history.push('/')
+        if (value === 2) history.push('/')
+        if (value === 3) history.push('/')
+    }, [value])
 
     return (
         <Box sx={{ width: "100%", position: "fixed", bottom: 0, backgroundColor: "#2d313a", zIndex: 100, }}>
